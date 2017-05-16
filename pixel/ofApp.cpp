f82c8f02aa4d192557eebc3192baa4e00a62baed
kim <azuremous@gmail.com>
2017-05-16 16:54:23 +0900

    class
    Use class for pixel

--- pixel/ofApp.cpp
+++ pixel/ofApp.cpp
@@ -7,9 +7,11 @@ void ofApp::setup(){
     
     widthSize = ofGetWidth() / size;
     heightSize = ofGetHeight() / size;
+    total =  widthSize * heightSize;
     
+    obj.resize(total);
     gap = (video.getWidth() - video.getHeight())/2;
-   
+    
 }
 
 //--------------------------------------------------------------
@@ -18,6 +20,12 @@ void ofApp::update(){
     video.update();
     if(video.isFrameNew()){
         p = video.getPixels();
+        
+        if(p.size() > 0){
+            for (pixelObject &o: obj) {
+                o.update();
+            }
+        }
     }
     
     
@@ -29,6 +37,7 @@ void ofApp::draw(){
     if(p.size() > 0){
         int radius = size/2;
         int count = 0;
+        
         for (int y = 0; y < heightSize; y++) {
             for (int x = 0; x < widthSize; x++) {
                 float rx = ofGetWidth() - (x*size + radius);//mirror
@@ -36,25 +45,7 @@ void ofApp::draw(){
                 int n = x + y * widthSize;
                 int cx = (int)ofMap(x * radius, 0, ofGetWidth()/2, gap, 640 - gap);
                 int cy = (int)ofMap(y * radius, 0, ofGetHeight()/2, 0, 480);
-                ofSetColor(p.getColor(cx, cy));
-                switch (count) {
-                    case 0:
-                        ofDrawCircle(rx, ry, radius);
-                        break;
-                        
-                    case 1:
-                        ofDrawRectangle(rx, ry, radius, radius);
-                        break;
-                        
-                    case 2:
-                        ofDrawLine(rx, ry, rx + radius, ry + radius);
-                        break;
-                        
-                    case 3:
-                        ofDrawRectangle(rx - radius, ry - radius, radius*2, radius*2);
-                        break;
-                        
-                }
+                obj[n].render(count, rx, ry, radius, p.getColor(cx, cy));
                 count++;
                 if(count > 4) count = 0;
             }
@@ -65,55 +56,57 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    
+    for (pixelObject &o: obj) {
+        o.roll();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
--- pixel/ofApp.h
+++ pixel/ofApp.h
@@ -2,6 +2,70 @@
 
 #include "ofMain.h"
 
+
+class pixelObject{
+public:
+    float spd;
+    float degree;
+    int timer;
+    int resetTime;
+    bool reset;
+    
+    pixelObject():degree(0.),reset(false)
+    {
+        spd = ofRandom(0.25, 1.2);
+        resetTime = ofRandom(0.3, 2.) * 100;
+    }
+    
+    void update(){
+        if(!reset){
+            degree += spd;
+        }else{
+            degree -= spd * 4;
+            if(degree <= 0) {
+                degree = 0;
+                timer++;
+                if(timer > resetTime){
+                    reset = false;
+                }
+            }
+        }
+    }
+    
+    void render(int type, float x, float y, float size, ofColor c){
+        ofPushMatrix();
+        ofTranslate(x, y);
+        ofRotate(degree);
+        ofTranslate(-size/2., -size/2.);
+        ofSetColor(c);
+        switch (type) {
+            case 0:
+                ofDrawCircle(0, 0, size);
+                break;
+                
+            case 1:
+                ofDrawRectangle(0, 0, size, size);
+                break;
+                
+            case 2:
+                ofDrawLine(0, 0, size, size);
+                break;
+                
+            case 3:
+                ofDrawRectangle(- size, - size, size*2, size*2);
+                break;
+                
+        }
+        ofPopMatrix();
+    }
+    
+    void roll(){
+        reset = true;
+        timer = 0;
+    }
+    
+};
+
 class ofApp : public ofBaseApp{
     
 public:
@@ -21,8 +85,11 @@ public:
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
     
+    vector<pixelObject>obj;
+    
     int widthSize;
     int heightSize;
+    int total;
     int gap;
     int size = 20;
     
